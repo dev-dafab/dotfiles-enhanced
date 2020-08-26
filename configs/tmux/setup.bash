547838180cc4ddad6ff609b400e63d804ac2af1d
Fabrice Dufils Siyapdje <dev.dafab@gmail.com>
2020-08-26 09:50:23 +0200

    update tmux setup

--- configs/tmux/setup.bash
+++ configs/tmux/setup.bash
@@ -1,14 +1,12 @@
 #!/usr/bin/env bash
 
 
-TMUX_PLUGIN_DIR="~/.tmux/plugins"
+TMUX_PLUGIN_DIR="$HOME/tmux/plugins"
 TMUX_PLUGIN_MANAGER_DIR="$TMUX_PLUGIN_DIR/tpm"
 TMUX_PLUGIN_MANAGER_INSTALL_SCRIPT="$TMUX_PLUGIN_MANAGER_DIR/bin/install_plugins"
 
-[[ -d "$TMUX_PLUGIN_MANAGER_DIR" ]] && mkdir -p "$TMUX_PLUGIN_MANAGER_DIR"
-
 function setup() {
-  if [[ -d "$TMUX_PLUGIN_MANAGER_DIR" ]]; then
+  if [[ ! -d "$TMUX_PLUGIN_MANAGER_DIR" ]]; then
     git clone https://github.com/tmux-plugins/tpm $TMUX_PLUGIN_MANAGER_DIR
     # shellcheck disable=SC1091,SC1090
     source "${TMUX_PLUGIN_MANAGER_INSTALL_SCRIPT}"
